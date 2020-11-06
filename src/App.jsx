645cfa8863cad888eee67d8f002ce639137fb35e
Mika Tonteri <mikaton@protonmail.com>
2020-11-06 10:31:27 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React SPA Starter</h1>
+      <p>Lorem Ipsum etcetera</p>
+    </div>
+  );
+}
+
+export default App;
